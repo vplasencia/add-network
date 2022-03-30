c2b41f34328a765b5291279973f5c3075080d2ee
vplasencia <v.pcalana@gmail.com>
2022-03-30 10:55:13 -0400

    Add meta tags

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,10 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Add Network</title>
+        <meta property="og:title" content="Add network to MetaMask" key="title" />
+      </Head>
       <div className="text-center mb-20 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-orange-600 to-red-600">
         <span>Add network to MetaMask</span>
       </div>
